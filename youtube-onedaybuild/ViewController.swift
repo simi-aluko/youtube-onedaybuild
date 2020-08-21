0fef993b47165b571338a527ee073c888841acc0
simileoluwa-aluko <simialuko24@gmail.com>
2020-08-21 08:46:53 +0100

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Simileoluwa Aluko on 21/08/2020.
+//  Copyright © 2020 Simileoluwa Aluko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
